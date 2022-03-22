07bee93c745ddf1b712f692c02085e3278f5bece
Jamehzz <probableeme@gmail.com>
2022-03-22 18:13:13 -0400

    added findAllEmployees method

--- db/schema.sql
+++ db/schema.sql
@@ -6,15 +6,15 @@ USE employees;
 
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    deptName VARCHAR(30)
+    name VARCHAR(30)
 );
 
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
-    FOREIGN KEY department_id
+    department_id INT,
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE CASCADE
 );
@@ -23,13 +23,13 @@ CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT
-    FOREIGN KEY role_id
+    role_id INT,
+    FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE CASCADE,
 
-    manager_id INT
-    FOREIGN KEY manager_id
+    manager_id INT,
+    FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
 );
