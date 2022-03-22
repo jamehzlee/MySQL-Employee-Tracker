bf2e6352aeb29c8195f63d58f45747fb1f3c9ccc
Jamehzz <probableeme@gmail.com>
2022-03-22 10:04:29 -0400

    added scripts and dependencies

--- db/schema.sql
+++ db/schema.sql
@@ -14,16 +14,18 @@ CREATE TABLE role(
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
-    FOREIGN KEY id
+    FOREIGN KEY department_id
     REFERENCES department(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
+    role_id INT
     FOREIGN KEY role_id
     REFERENCES role(id)
+    ON DELETE CASCADE,
+    manager_id INT
 );
